b1831863a4d1add34e1796882f0c896c9b062d5d
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-31 06:06:38 +0300

    COMPLETED: Sinatra React Lab-Chatterbox

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,36 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
-  
+  #GET
+  get '/messages' do
+    messages = Message.all.order(:created_at)
+    messages.to_json
+  end
+
+  #POST
+  post '/messages' do
+    message = Message.create(
+      username: params[:username],
+      body: params[:body]
+    )
+    message.to_json
+  end
+
+  #PATCH
+  patch '/messages/:id' do
+    message = Message.find(params[:id])
+    message.update(
+      body: params[:body]
+    )
+    message.to_json
+  end
+
+  #DELETE
+  delete '/messages/:id' do 
+    message = Message.find(params[:id])
+
+    message.destroy
+
+    message.to_json
+  end
 end
